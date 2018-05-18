2a20663c6d73ae0d4004a1d12f193cb426af9707
Aditi Puntambekar <apuntamb@redhat.com>
2018-05-18 19:20:22 +0530

    A simple Ruby class Person that demonstrates use of get_ and set_ methods

---
+++ class2.rb
@@ -0,0 +1,25 @@
+# A program to demonstrate use of get_ and set_ methods
+
+class Person
+
+	#setter methods
+
+	def set_name(name)
+		@name=name
+	end
+
+	#getter methods
+
+	def get_name
+		@name
+	end
+
+end
+
+employee1 = Person.new
+employee1.set_name("Peter")
+puts employee1.get_name
+
+employee2 = Person.new
+employee2.set_name("Tom")
+puts employee2.get_name
